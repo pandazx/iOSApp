f7c61f5b8ff2a8433a413ec2c608d1ce2d1c730c
pandazx <pandazx9r@gmail.com>
2014-11-03 15:52:14 +0900

    getLocation(lat/long)

--- iOSApp/ViewController.swift
+++ iOSApp/ViewController.swift
@@ -7,25 +7,69 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
-    @IBOutlet var label:UILabel!
+    var lm:CLLocationManager
+    var longitude: CLLocationDegrees
+    var latitude: CLLocationDegrees
+    
+    @IBOutlet var lonLabel: UILabel!
+    @IBOutlet var latLabel: UILabel!
 
+    required init(coder aDecoder: NSCoder) {
+        lm = CLLocationManager()
+        longitude = CLLocationDegrees()
+        latitude = CLLocationDegrees()
+        super.init(coder: aDecoder)
+    }
+    
+    @IBAction func btnGetLocation(sender: AnyObject) {
+        // get lat and long
+        lm.startUpdatingLocation()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    @IBAction func button(){
-        label.text = "Hello world!"
+        
+        // 現在地の取得
+        lm = CLLocationManager()
+        lm.delegate = self
+        
+        // セキュリティ認証のステータスを取得
+        let status = CLLocationManager.authorizationStatus()
+        
+        // まだ認証が得られていない場合は、認証ダイアログを表示
+        if(status == CLAuthorizationStatus.NotDetermined) {
+            println("didChangeAuthorizationStatus:\(status)");
+            // まだ承認が得られていない場合は、認証ダイアログを表示
+            self.lm.requestAlwaysAuthorization()
+        }
+        
+        // 取得精度の設定
+        lm.desiredAccuracy = kCLLocationAccuracyBest
+        // 取得頻度の設定
+        lm.distanceFilter = 100
     }
 
+    // 位置情報取得成功時
+    func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!){
+        
+        longitude = newLocation.coordinate.longitude
+        latitude = newLocation.coordinate.latitude
+        self.lonLabel.text = "\(longitude)"
+        self.latLabel.text = "\(latitude)"
+    }
+    
+    // 位置情報取得失敗時
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        NSLog("Error while updating location. " + error.localizedDescription)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
